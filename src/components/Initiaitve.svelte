0b4f56a89e0e1061d1ff5fd7c43a83a877d3ceb2
Jim Krayer <jameskrayer@yahoo.com>
2022-03-18 14:41:04 -0400

    Die roll component

--- src/components/Initiaitve.svelte
+++ src/components/Initiaitve.svelte
@@ -1,25 +1,44 @@
 <script>
-    import character from '../stores/Character'
-    import { rollFormula } from '../lib'
+  import Die from './Die.svelte';
+  import character from '../stores/Character'
+  import { rollFormula } from '../lib'
 
-    const { abilities } = $character;
+  const { abilities } = $character;
 
-    // Handlers
-    const handleRoll = () => $character.initiative = rollFormula([1, 'd', 6, '+', abilities.agility])
+  let result = null;
+  let roll = 1;
+
+  // Handlers
+  const handleRoll = () => {
+      roll = rollFormula([1, 'd', 6]);
+      result = roll + abilities.agility;
+      $character.initiative = result;
+  }
 </script>
 
-<div class="initiative row" on:click={handleRoll}>
-    <div>
-        <div class="score-name">
-            <abbr title="Initiative">Init</abbr>
-        </div>
-        <div
-            class="score"
-        >
-          {$character.initiative || 0}
+<Die
+  onClose={() => result = null}
+  result={result}
+  message={{
+    formula: '1d6',
+    scoreName: 'Agility',
+    score: abilities.agility,
+    roll
+  }}
+>
+    <div class="initiative row" on:click={handleRoll}>
+        <div>
+            <div class="score-name">
+                <abbr title="Initiative">Init</abbr>
+            </div>
+            <div
+                class="score"
+            >
+            {$character.initiative || 0}
+            </div>
         </div>
     </div>
-</div>
+</Die>
 
 <style>
 .initiative {
