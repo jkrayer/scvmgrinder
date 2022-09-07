115daf234cd09cc0040e996540ed574ceb082213
Jim Krayer <jameskrayer@yahoo.com>
2022-09-07 16:37:51 -0400

    Add crit messaging

--- src/lib/character/attack.ts
+++ src/lib/character/attack.ts
@@ -1,6 +1,8 @@
+// SIDE EFFECTS
 import { breakWeaponsAndArmor } from "./equipment";
 import { getAbilityScore, rollMessage } from "./common";
 import { rollD20 } from "../dice";
+import { ATTACK_CRIT } from "../game_constants";
 import { update } from "../../CharacterSheet/store";
 import { rollFormula } from "../../Messages/lib";
 import { addMessage } from "../../Messages/state/MessageStore";
@@ -17,14 +19,15 @@ export function attack(character: CharacterType, weapon: Weapon) {
   const modName: AttackScores = ATTACK[subType];
   const mod: number = getAbilityScore(character, modName);
   const roll: number = rollD20();
-  const rollType: string = rollMessage(roll);
+  const critOrFail: string = rollMessage(roll);
+  const crit: string = roll === 20 ? `. ${ATTACK_CRIT}` : "";
 
   const message: MessageBody = {
     name: character.name,
     rollType: "To Hit",
     roll: roll + mod,
     rollFormula: rollFormula(roll, mod),
-    target: `${weapon.name}${rollType}`,
+    target: `${weapon.name}${critOrFail}${crit}`,
     dc: null,
   };
 
--- src/lib/game_constants/index.ts
+++ src/lib/game_constants/index.ts
@@ -1,10 +1,15 @@
 export * as Status from "./status";
 
-export const BASE_ENCUMBRANCE = 8;
+export const BASE_ENCUMBRANCE: number = 8;
 
-export const ATTACK_DC = 12;
+export const ATTACK_DC: number = 12;
 
-export const DEFENSE_DC = 12;
+export const DEFENSE_DC: number = 12;
+
+export const ATTACK_CRIT: string =
+  "Double damage and target's armor is reduced one tier.";
+
+export const DEFENSE_CRIT: string = " gains a free attack.";
 
 export const POWERS = Object.freeze({
   text: "Presence DR12, or -d2 HP and no Powers for 1 hr.",
