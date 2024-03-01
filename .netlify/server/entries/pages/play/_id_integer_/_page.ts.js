fdca0f5a491b2e2234252c39a0ba7ecf4b11e715
Jim Krayer <jameskrayer@yahoo.com>
2024-03-01 18:21:35 -0500

    More deleting netlify

--- .netlify/server/entries/pages/play/_id_integer_/_page.ts.js
+++
@@ -1,9 +0,0 @@
-import { b as getCharacter } from "../../../../chunks/db.js";
-async function load({
-  params: { id }
-}) {
-  return { character: await getCharacter(parseInt(id, 10)) };
-}
-export {
-  load
-};
